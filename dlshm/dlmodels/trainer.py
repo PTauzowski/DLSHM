6903c15049cda032898fdd5d7a912cf68f094164
piotrek <ptauzow@ippt.pan.pl>
2025-01-17 07:59:48 +0100

    200 Epochs weighted

--- dlshm/dlmodels/trainer.py
+++ dlshm/dlmodels/trainer.py
@@ -1,11 +1,16 @@
 import time
 import os
+from tabnanny import verbose
+
 import matplotlib
 import cv2 as cv
 import tensorflow as tf
 import numpy as np
+from flatbuffers.packer import float32
+from keras.src.metrics.accuracy_metrics import accuracy, categorical_accuracy
 from matplotlib import image as mpimg, pyplot as plt
-import keras.backend as K
+from tensorflow.python.ops.metrics_impl import mean_iou
+import pandas as pd
 
 # import datetime as dt
 
@@ -50,15 +55,15 @@ class DLTrainer:
         test_path = os.path.join(test_path, self.model_name)
         if not os.path.exists(test_path):
             os.makedirs(test_path)
-        index=1
         N=len(test_gen)
         print('Testing model:',self.model_name)
-        for x,y in test_gen:
-            data_y = self.model.predict(x)
-            postprocess(os.path.join(test_path, 'test_file_'+str(index))+'.'+extension,x,y,data_y)
-            index=index+1
-            if index % 100 == 0:
-                print('iter=', index, '/', N, flush=True)
+        for i, (x, y) in enumerate(test_gen):
+            data_y = self.model.predict(x,verbose=0)
+            postprocess(os.path.join(test_path, 'test_file_'+str(i))+'.'+extension,x,y,data_y)
+            if i % 100 == 0:
+                print('iter=', i, '/', N, flush=True)
+            if i + 1 >= N:  # Stop after all batches
+                break
 
     def test_model_on_images(self, test_gen, postprocess, extension='png'):
         test_path=os.path.join(self.task_path, 'TestResults')
@@ -68,7 +73,7 @@ class DLTrainer:
         index=1
         N=len(test_gen)
         print('Testing model:',self.model_name)
-        for x,y in test_gen:
+        for i, (x, y) in enumerate(test_gen):
             data_y = self.model.predict(x)
             cv.imwrite(os.path.join(test_path, 'test_file_T_' + str(index))+'.'+extension, postprocess(x[0,],data_y[0,])*255)
             cv.imwrite(os.path.join(test_path, 'test_file_Y_' + str(index)) + '.' + extension, y[0,]*255)
@@ -76,6 +81,8 @@ class DLTrainer:
             index=index+1
             if index % 100 == 0:
                 print('iter=', index, '/', N, flush=True)
+            if i + 1 >= N:  # Stop after all batches
+                break
 
 
     def test_model_weighted(self, test_gen, postprocess, extension='png'):
@@ -92,6 +99,8 @@ class DLTrainer:
             index=index+1
             if index % 100 == 0:
                 print('iter=', index, '/', N, flush=True)
+            if index + 1 >= N:  # Stop after all batches
+                break
 
     def predict(self,img_source, inputImgReader, postprocess):
         predictions_dir=os.path.join(self.task_path, 'Predictions')
@@ -171,91 +180,148 @@ class DLTrainer:
         axis[1].set_title('Accuracy Comparison')
         axis[1].legend()
 
+    def compute_measures(self, data_y, true_y, weights):
+        n_masks =  true_y.shape[2]
+        accuracy_fn = tf.keras.metrics.Accuracy()
+        cce_loss = tf.keras.losses.CategoricalCrossentropy()
+        accuracy  = np.zeros((n_masks), dtype=np.float32)
+        keras_accuracy = np.zeros((n_masks), dtype=np.float32)
+        cce_value = np.zeros((n_masks), dtype=np.float32)
+        iou = np.zeros((n_masks), dtype=np.float32)
+        precision = np.zeros((n_masks), dtype=np.float32)
+        true_positives = np.zeros((n_masks), dtype=np.float32)
+        true_negatives = np.zeros((n_masks), dtype=np.float32)
+        false_negatives = np.zeros((n_masks), dtype=np.float32)
+        false_positives = np.zeros((n_masks), dtype=np.float32)
+        all_pixels = true_y[:,:,0].size
+        epsilon=1.0E-7
 
+        for k in range(0,n_masks):
+            true_positives[k]   = np.sum(     true_y[:, :, k]  *      data_y[ :, :, k])
+            true_negatives[k]   = np.sum((1 - true_y[:, :, k]) * (1 - data_y[ :, :, k]))
+            false_positives[k]  = np.sum((1 - true_y[:, :, k]) *      data_y[ :, :, k])
+            false_negatives[k]  = np.sum(     true_y[:, :, k]  * (1-  data_y[ :, :, k]))
+            accuracy[k] = np.sum(true_y[:, :, k] * data_y[ :, :, k]) / np.sum(true_y[:, :, k])
+            keras_accuracy[k] = accuracy_fn(true_y[:, :, k], data_y[ :, :, k])
+            cce_value[k] = cce_loss(true_y[:, :, k], data_y[ :, :, k])
 
-    def compute_measures(self, img_source, inputImgReader, postprocess):
-        predictions_dir=os.path.join(self.task_path, 'Predictions')
-        if not os.path.exists(predictions_dir):
-            os.makedirs(predictions_dir)
-        prediction_path = os.path.join(predictions_dir, self.model_name)
-        if not os.path.exists(prediction_path):
-            os.makedirs(prediction_path)
-        index=1
-        print('Predicting images from dir:', img_source)
-        N = len(os.listdir(img_source))
-        for filename in os.listdir(img_source):
-            try:
-                data_x = inputImgReader(os.path.join(img_source, filename))
-                true_y = cv.imread(os.path.join(img_source, 'label_'+filename))
-                data_y = self.model.predict(np.expand_dims(data_x,0))
+        precision = true_positives / (true_positives + false_positives+epsilon)
+        recall = true_positives / (true_positives + false_negatives + epsilon)
+        dice_f1 = 2 * true_positives / (precision+recall + epsilon)
+        dsc = 2 * true_positives / ( 2 * true_positives + false_positives + false_negatives + epsilon)
+        specificity = true_negatives / ( true_negatives + false_positives + epsilon)
+        balanced_accuracy = (recall + specificity) / 2
+        iou = true_positives / (true_positives + false_positives + false_negatives + epsilon)
+        m_iou = np.sum(iou) / n_masks
+        global_iou = np.sum(true_positives) / (np.sum(true_positives+false_positives+false_negatives)+epsilon)
+        return true_positives, true_negatives, false_positives, false_negatives, accuracy, keras_accuracy, cce_value, precision, recall, dice_f1, dsc, specificity, balanced_accuracy, iou, m_iou, global_iou
 
-                threshold = 0.5
+    def compute_binary_measures(self, data_y, true_y, weights):
+        n_masks = true_y.shape[2]
+        if weights.size != n_masks:
+            weights = np.ones((n_masks),float32)
 
+        true_positives = np.zeros((n_masks), dtype=int)
+        true_negatives = np.zeros((n_masks), dtype=int)
+        false_negatives = np.zeros((n_masks), dtype=int)
+        false_positives = np.zeros((n_masks), dtype=int)
 
-                mask = np.empty((320, 640, 8), dtype=np.float32)
-                mask[:, :, 0] = np.where(true_y[:,:,1] == 1, 1, 0)
-                mask[:, :, 1] = np.where(true_y[:,:,1] == 2, 1, 0)
-                mask[:, :, 2] = np.where(true_y[:,:,1] == 3, 1, 0)
-                mask[:, :, 3] = np.where(true_y[:,:,1] == 4, 1, 0)
+        for k in range(0,n_masks):
+            true_positives[k] = np.sum((data_y[:, :, k] == 1) & (true_y[:, :, k] == 1))
+            true_negatives[k] = np.sum((data_y[:, :, k] == 0) & (true_y[:, :, k] == 0))
+            false_negatives[k] = np.sum((data_y[:, :, k] == 0) & (true_y[:, :, k] == 1))
+            false_positives[k] = np.sum((data_y[:, :, k] == 1) & (true_y[:, :, k] == 0))
 
-                accuracy = np.sum(mask[:, :, 1]  * data_y[0,:,:,1])/np.sum(mask[:, :, 2] )
+        precision = true_positives / (true_positives + false_positives)
+        recall = true_positives / (true_positives + false_negatives)
+        dice_f1 = 2 * true_positives / (precision + recall)
+        dsc = 2 * true_positives / (2 * true_positives + false_positives + false_negatives)
+        specificity = true_negatives / (true_negatives + false_positives)
+        balanced_accuracy = (recall + specificity) / 2
+        iou = true_positives / (true_positives + false_positives + false_negatives)
+        m_iou = np.sum(iou) / n_masks
+        global_iou = np.sum(true_positives) / np.sum(true_positives + false_positives + false_negatives)
+        return true_positives, true_negatives, false_positives, false_negatives, accuracy, precision, recall, dice_f1, dsc, specificity, balanced_accuracy, iou, m_iou, global_iou
 
-                # correct_predictions = np.sum(pred_mask == true_mask)
-                # total_pixels = pred_mask.size
-                # accuracy = correct_predictions / total_pixels
+    def compute_gen_measures(self, sample_generator, weights, CLASS_NAMES):
+        N = len(sample_generator)
+        n_masks = weights.size
+        epsilon = 1.0E-7
+        index=0
+        threshold = 0.5
 
-                idx_pred=2
-                idx_mask=0
+        TP = np.zeros((n_masks), dtype=np.float64)
+        TN = np.zeros((n_masks), dtype=np.float64)
+        FN = np.zeros((n_masks), dtype=np.float64)
+        FP = np.zeros((n_masks), dtype=np.float64)
 
-                cce_loss = tf.keras.losses.CategoricalCrossentropy()
-                cce_value = cce_loss(mask[:, :, idx_mask], data_y[0, :, :, idx_pred])
+        binTP = np.zeros((n_masks), dtype=np.uint64)
+        binTN = np.zeros((n_masks), dtype=np.uint64)
+        binFN = np.zeros((n_masks), dtype=np.uint64)
+        binFP = np.zeros((n_masks), dtype=np.uint64)
 
-                mean_iou_fn = tf.keras.metrics.MeanIoU(num_classes=1)
-                mean_iou_fn.update_state(mask[:, :, idx_mask], data_y[0, :, :, idx_pred])
-                keras_iou=mean_iou_fn.result().numpy()
+        for i, (data_x, true_y) in enumerate(sample_generator):
+            data_y = self.model.predict(data_x, verbose=0)
+            binary_mask = (true_y >= threshold).astype(int)
+            binary_pred = (data_y >= threshold).astype(int)
 
+            for k in range(0, n_masks):
+                TP[k] = TP[k] + np.sum(true_y[0, :, :, k] * data_y[0, :, :, k])
+                TN[k] = TN[k] + np.sum((1 - true_y[0, :, :, k]) * (1 - data_y[0, :, :, k]))
+                FP[k] = FP[k] + np.sum((1 - true_y[0, :, :, k]) * data_y[0, :, :, k])
+                FN[k] = FN[k] + np.sum(true_y[0, :, :, k] * (1 - data_y[0, :, :, k]))
+                binTP[k] = binTP[k] + np.sum((binary_pred[0, :, :, k] == 1) & (binary_mask[0, :, :, k] == 1))
+                binTN[k] = binTN[k] + np.sum((binary_pred[0, :, :, k] == 0) & (binary_mask[0, :, :, k] == 0))
+                binFN[k] = binFN[k] + np.sum((binary_pred[0, :, :, k] == 0) & (binary_mask[0, :, :, k] == 1))
+                binFP[k] = binFP[k] + np.sum((binary_pred[0, :, :, k] == 1) & (binary_mask[0, :, :, k] == 0))
 
-
-                accuracy_fn = tf.keras.metrics.Accuracy()
-                keras_accuracy = accuracy_fn(mask[:, :, idx_mask], data_y[0, :, :, idx_pred])
-
-
-                binary_mask = (mask[:, :, idx_mask] >= threshold).astype(int)
-                binary_pred = (data_y[0, :, :, idx_pred] >= threshold).astype(int)
-                TP = np.sum((binary_pred == 1) & (binary_mask == 1))
-                FP = np.sum((binary_pred == 1) & (binary_mask == 0))
-                FN = np.sum((binary_pred == 0) & (binary_mask == 1))
-                TN = np.sum((binary_pred == 0) & (binary_mask == 0))
-
-                correct_predictions = np.sum(mask[:, :, idx_mask] * data_y[0, :, :, idx_pred])
-                correct_negative_predictions = np.sum((1-mask[:, :, idx_mask]) * (1-data_y[0, :, :, idx_pred]))
-                false_negative_predictions = np.sum((1 - mask[:, :, idx_mask]) * data_y[0, :, :, idx_pred])
-                false_positive_predictions = np.sum(mask[:, :, idx_mask] * (1 - data_y[0, :, :, idx_pred]))
-                total_pixels = binary_pred.size
-                accuracy = (correct_predictions + correct_negative_predictions ) / total_pixels
-                iou = correct_predictions / (correct_predictions + false_positive_predictions + false_negative_predictions )
-
-                #intersection = np.sum(mask[:, :, idx_mask] * data_y[0, :, :, idx_pred])
-
-                # true positive / (true positive + false positive + false negative)
-
-                intersection = np.sum(data_y[0, :, :, idx_pred] * mask[:, :, idx_mask])
-                union = np.sum(np.maximum(binary_mask, binary_pred))
-                iou = intersection / union # if union != 0 else 0  # To
-                # Calculate IoU
-                bin_iou = TP / (TP + FP + FN) if (TP + FP + FN) != 0 else 0  # To avoid division by zero
-
-                # Calculate accuracy
-                bin_accuracy = (TP + TN) / (TP + FP + FN + TN)  # All pixels
-
-                print("IoU:", bin_iou)
-                print("Accuracy:", bin_accuracy)
-
-            except Exception as e:
-                print('Cant import ' + filename + ' because', e)
             index = index + 1
             if index % 100 == 0:
                 print('iter=', index, '/', N, flush=True)
+            if index + 1 >= N:  # Stop after all batches
+                break
+        class_accuracy = (TP + TN) / ( TP + TN + FP + FN + epsilon)
+        cat_accuracy = np.sum((TP + TN) / ( TP + TN + FP + FN + epsilon) ) / n_masks
+        global_accuracy = np.sum(TP + TN) / (np.sum(TP + TN + FP + FN) + epsilon)
+        weighted_accuracy = np.sum( weights * (TP + TN) / ( TP + TN + FP + FN + epsilon) )
+
+        precision = TP / (TP + FP + epsilon)
+        recall = TP / (TP + FN + epsilon)
+        dice_f1 = 2 * TP / (precision + recall + epsilon)
+        dsc = 2 * TP / (2 * TP + FP + FN +epsilon)
+        specificity = TN / (TN + FP)
+        balanced_accuracy = (recall + specificity) / 2
+        iou = TP / (TP + FP + FN + epsilon)
+        mean_iou = np.sum(iou) / n_masks
+        global_iou = np.sum(TP) / np.sum(TP + FP + FN + epsilon)
+
+        bin_class_accuracy = (binTP + binTN) / (binTP + binTN + binFP + binFN + epsilon)
+        bin_cat_accuracy = np.sum((binTP + binTN) / (binTP + binTN + binFP + binFN + epsilon))
+        bin_global_accuracy = np.sum(binTP + binTN) / (np.sum(binTP + binTN + binFP + binFN) + epsilon)
+        bin_weighted_accuracy = np.sum(weights * (binTP + binTN) / (binTP + binTN + binFP + binFN + epsilon))
+
+        bin_precision = binTP / (binTP + binFP + epsilon)
+        bin_recall = binTP / (binTP + binFN + epsilon)
+        dice_f1 = 2 * binTP / (bin_precision + bin_recall + epsilon)
+        dsc = 2 * binTP / (2 * binTP + binFP + binFN + epsilon)
+        bin_specificity = binTN / (binTN + binFP)
+        balanced_accuracy = (bin_recall + bin_specificity) / 2
+        bin_iou = binTP / (binTP + binFP + binFN + epsilon)
+        mean_bin_iou = np.sum(bin_iou) / n_masks
+        global_bin_iou = np.sum(binTP) / np.sum(binTP + binFP + binFN + epsilon)
+
+            # DataFrame 1
+        # Categorical accuracy
+        # 'Global accuracy':
+        # 'Weighted accuracy' :
+        # 'Mean IOU'
+        # 'Global IOU'
+
+
+        dfs = pd.DataFrame({'Categorical accuracy':[cat_accuracy], 'Global accuracy': [global_accuracy], 'Weighted accuracy' : [weighted_accuracy], 'Mean IOU' : [mean_iou], 'Global IOU' : [global_iou] })
+        df = pd.DataFrame({'Class': CLASS_NAMES, 'Class accuracy': class_accuracy, 'Precision': precision, 'Recall' : recall, 'F1 Score (Dice Coefficient)': dsc, 'Specificity':specificity, 'Balanced accuracy':balanced_accuracy, 'IOU': iou})
+        return dfs, df
+
 
 
 
