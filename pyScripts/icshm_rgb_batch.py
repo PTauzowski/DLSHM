33ec558859eb59f31003b1e7f7a68c3cec7de356
piotrek <ptauzow@ippt.pan.pl>
2025-03-18 06:32:05 +0100

    Separate augmentation, out of memory prevent

--- pyScripts/icshm_rgb_batch.py
+++ pyScripts/icshm_rgb_batch.py
@@ -64,136 +64,145 @@ model_deeplabv3p101 = DeeplabV3Plus101((RES_Y, RES_X, N_CHANNELS), N_CLASSES)
 
 
 def rgb_model_function( model_name, model, augment_fn, batch_size, epochs):
-    CURRENT_MODEL_NAME = model_name
+    try:
+        CURRENT_MODEL_NAME = model_name
 
-    #CURRENT_MODEL_NAME= 'ICSHM_RGB_DEEPLABV3_100'
-    #CURRENT_MODEL_NAME= 'ICSHM_RGB_DEEPLABV3p_150a2'
-    #CURRENT_MODEL_NAME= 'ICSHM_RGB_UNET_100'
-    #CURRENT_MODEL_NAME= 'ICSHM_RGB_VGG19_100'
+        #CURRENT_MODEL_NAME= 'ICSHM_RGB_DEEPLABV3_100'
+        #CURRENT_MODEL_NAME= 'ICSHM_RGB_DEEPLABV3p_150a2'
+        #CURRENT_MODEL_NAME= 'ICSHM_RGB_UNET_100'
+        #CURRENT_MODEL_NAME= 'ICSHM_RGB_VGG19_100'
 
-    if User=='Mariusz':
-        TASK_PATH = "D:/Datasets/Tokaido_Dataset" # sys.argv[1]
-        TRAIN_IMAGES_PATH = TASK_PATH + '/' + 'DL4SHM_trainSet'
-        MODEL_PATH = TASK_PATH + '/' + CURRENT_MODEL_NAME
-        IMAGES_SOURCE_PATH = 'D:/Datasets/Tokaido_Dataset'
-        PREDICT_DIR = 'F:/Python/DL4SHM_results/Predictions'
-        TEST_PATH = 'F:/Python/DL4SHM_results' + '/' + 'Test'
+        if User=='Mariusz':
+            TASK_PATH = "D:/Datasets/Tokaido_Dataset" # sys.argv[1]
+            TRAIN_IMAGES_PATH = TASK_PATH + '/' + 'DL4SHM_trainSet'
+            MODEL_PATH = TASK_PATH + '/' + CURRENT_MODEL_NAME
+            IMAGES_SOURCE_PATH = 'D:/Datasets/Tokaido_Dataset'
+            PREDICT_DIR = 'F:/Python/DL4SHM_results/Predictions'
+            TEST_PATH = 'F:/Python/DL4SHM_results' + '/' + 'Test'
 
-    elif User=="Piotr":
-        TASK_PATH = "/home/piotrek/Computations/Ai/ICSHM" # sys.argv[1]
-        #TASK_PATH = "h:\\DL\\ICSHM"  # sys.argv[1]
-        MODEL_PATH = TASK_PATH + '/' + CURRENT_MODEL_NAME
-        #IMAGES_SOURCE_PATH = '/Users/piotrek/DataSets/Tokaido_dataset_share'
-        IMAGES_SOURCE_PATH = '/home/piotrek/Computations/Ai/Data/Tokaido_dataset_share'
-        #IMAGES_SOURCE_PATH = '/Users/piotrek/Computations/Ai/Data/Tokaido_dataset_share'
-        #IMAGES_SOURCE_PATH = 'h:\\DL\\ICSHM\\DataSets\\Tokaido_dataset_share'
-        PREDICTIONS_PATH=os.path.join( MODEL_PATH, 'Predictions' )
-        #TRAIN_IMAGES_PATH= TASK_PATH + '/' + 'TrainSets/RGB'
-        TRAIN_IMAGES_PATH = '/home/piotrek/Computations/Ai/ICSHM/TrainSet4'
-        TEST_PATH = MODEL_PATH + '/' + 'Test'
-
-
-    # info_fil
-    # e = pd.read_csv(data_info_file, header=None, index_col=None, delimiter=',')
+        elif User=="Piotr":
+            TASK_PATH = "/home/piotrek/Computations/Ai/ICSHM" # sys.argv[1]
+            #TASK_PATH = "h:\\DL\\ICSHM"  # sys.argv[1]
+            MODEL_PATH = TASK_PATH + '/' + CURRENT_MODEL_NAME
+            #IMAGES_SOURCE_PATH = '/Users/piotrek/DataSets/Tokaido_dataset_share'
+            IMAGES_SOURCE_PATH = '/home/piotrek/Computations/Ai/Data/Tokaido_dataset_share'
+            #IMAGES_SOURCE_PATH = '/Users/piotrek/Computations/Ai/Data/Tokaido_dataset_share'
+            #IMAGES_SOURCE_PATH = 'h:\\DL\\ICSHM\\DataSets\\Tokaido_dataset_share'
+            PREDICTIONS_PATH=os.path.join( MODEL_PATH, 'Predictions' )
+            #TRAIN_IMAGES_PATH= TASK_PATH + '/' + 'TrainSets/RGB'
+            TRAIN_IMAGES_PATH = '/home/piotrek/Computations/Ai/ICSHM/TrainSet4'
+            TEST_PATH = MODEL_PATH + '/' + 'Test'
 
 
-    class SegmentationRGBInputFileReader:   # Reading of the SOURCE images.
-        def __init__(self, resX, resY ):
-            self.x = np.zeros((resY, resX, 3), dtype=np.float32)
-            self.resX=resX
-            self.resY=resY
-
-        def __call__(self, filename):
-            image = cv.imread(filename)
-            image_array = resize(image, (self.resY, self.resX), anti_aliasing=True)
-            self.x[:, :, 0] = image_array[:, :, 0]
-            self.x[:, :, 1] = image_array[:, :, 1]
-            self.x[:, :, 2] = image_array[:, :, 2]
-            return self.x
-
-    def predictDMGsegmentation(x, y):  # wizualizacja masek z sieci
-        colors = np.array([
-            [0, 0, 0],  # background
-            [1, 0, 0],  # mask 1 (red)
-            [0, 1, 0],  # mask 2 (green)
-            [0, 0, 1]  # mask 3 (blue)
-        ], dtype=np.float32)
-
-        nmasks = y.shape[2]
-        masks = colors[np.argmax(y, axis=-1)]
-
-        alpha = 0.6
-        result= cv.addWeighted(masks, 1-alpha, x, alpha, 0)
-        return result
-
-    EPOCHS=epochs
-    BATCH_SIZE=batch_size
-
-    TRAIN_DATA_RATIO=0.8
-    TEST_DATA_RATIO=0.15
-    CROSS_VALIDATION_FOLDS=6
-
-    #CLASS_NAMES =["Nonbridge", "Slab", "Beam", "Column", "Nonstructural", "Rail", "Sleeper", "Other" ]
-
-    CLASS_NAMES =["Nonstructural", "Slab", "Beam", "Column" ]
-
-    #dir_files_processing('/Users/piotrek/Computations/Ai/ICSHM/Predictions/Photos/Images', ImageResizer(RES_X,RES_Y,'/Users/piotrek/Computations/Ai/ICSHM/Predictions/Photos/PredictionPhotos'))
-    imgRGB_conv  = ICSHM_RGB_4_Converter(RES_X, RES_Y)    # konwersja na pliki npy - jak sa, to juz tego nie robi
-    data_manager = ICSHMDataManager(IMAGES_SOURCE_PATH) # na razie nie wiadomo
-    data_manager.convert_data_to_numpy_format( imgRGB_conv, TRAIN_IMAGES_PATH )  # powinno sie nie uruchamiac, jak sa npy
-
-    # weights = compute_class_weights(y)
-    print("Class Weights:", data_manager.weights)
-    print("Sum   Weights:", sum(data_manager.weights))
-
-    # class weights reflects not only pixel ratio but also class importance
-    #class_weights = np.array([0.05, 0.1, 0.1, 0.1, 0.2, 0.5, 1.0, 0.05])
-
-    class_weights = np.array([0.07, 0.33, 0.35, 0.25])
-
-    # Normalize the weights so that their sum is 1
-    normalized_class_weights = class_weights / np.sum(class_weights)
-
-    loss_fn = weighted_categorical_crossentropy(normalized_class_weights)
+        # info_fil
+        # e = pd.read_csv(data_info_file, header=None, index_col=None, delimiter=',')
 
 
-    # Przetwarzanie danych do trenowania i stworzenie obiektu trenera (może być niepotrzebny)
-    dataSource = DataSource( TRAIN_IMAGES_PATH, train_ratio=0.7, validation_ratio=0.1, sampleSize=-1)
-    trainer = DLTrainer(CURRENT_MODEL_NAME, model, TASK_PATH)  # Tu wchodzi model, ale można dać "none" i będzie próbował model wydobyć z katalogu
+        class SegmentationRGBInputFileReader:   # Reading of the SOURCE images.
+            def __init__(self, resX, resY ):
+                self.x = np.zeros((resY, resX, 3), dtype=np.float32)
+                self.resX=resX
+                self.resY=resY
 
-    model=trainer.model  # Gdyby model powyżej nie był podany ("none" - jak w komentarzu), to tutaj go "wydobywamy"
+            def __call__(self, filename):
+                image = cv.imread(filename)
+                image_array = resize(image, (self.resY, self.resX), anti_aliasing=True)
+                self.x[:, :, 0] = image_array[:, :, 0]
+                self.x[:, :, 1] = image_array[:, :, 1]
+                self.x[:, :, 2] = image_array[:, :, 2]
+                return self.x
 
-    # Kompilacja modelu i wyswitlenie informacji:
-    #model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE), loss=loss_fn, metrics=[tf.keras.metrics.CategoricalAccuracy(), tf.keras.metrics.MeanIoU(N_CLASSES)])
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE), loss="categorical_crossentropy", metrics=[tf.keras.metrics.CategoricalAccuracy(), tf.keras.metrics.MeanIoU(N_CLASSES)])
-    model.summary()
+        def predictDMGsegmentation(x, y):  # wizualizacja masek z sieci
+            colors = np.array([
+                [0, 0, 0],  # background
+                [1, 0, 0],  # mask 1 (red)
+                [0, 1, 0],  # mask 2 (green)
+                [0, 0, 1]  # mask 3 (blue)
+            ], dtype=np.float32)
 
-    # Generatory danych do trenowania (podstawia dane, jak w tablicy) i walidacji:
-    train_gen = DataGeneratorFromNumpyFilesMem(dataSource.get_train_set_files(),BATCH_SIZE,(RES_Y,RES_X),(RES_Y,RES_X),N_CHANNELS,N_CLASSES, augmentation_fn=augment_fn)
-    validation_gen = DataGeneratorFromNumpyFilesMem(dataSource.get_validation_set_files(),1,(RES_Y,RES_X),(RES_Y,RES_X),N_CHANNELS,N_CLASSES)
-    test_gen = DataGeneratorFromNumpyFilesMem(dataSource.get_test_set_files(),1,(RES_Y,RES_X),(RES_Y,RES_X),N_CHANNELS,N_CLASSES)
+            nmasks = y.shape[2]
+            masks = colors[np.argmax(y, axis=-1)]
 
-    # Rozpoczęcie treningu (w używania wytrenowanego modelu komentujemy funkcje poniżej)
-    trainer.train(train_gen, validation_gen, EPOCHS, BATCH_SIZE)
-    trainer.plot_training_history()
+            alpha = 0.6
+            result= cv.addWeighted(masks, 1-alpha, x, alpha, 0)
+            return result
 
-    # Poniższe funkcje są używane tylko w przypadku trenowania nowych modeli
-    print("Evaluate on test data")
-    results = model.evaluate(test_gen, batch_size=1)
-    print("test results:", results)
+        EPOCHS=epochs
+        BATCH_SIZE=batch_size
+
+        TRAIN_DATA_RATIO=0.8
+        TEST_DATA_RATIO=0.15
+        CROSS_VALIDATION_FOLDS=6
+
+        #CLASS_NAMES =["Nonbridge", "Slab", "Beam", "Column", "Nonstructural", "Rail", "Sleeper", "Other" ]
+
+        CLASS_NAMES =["Nonstructural", "Slab", "Beam", "Column" ]
+
+        #dir_files_processing('/Users/piotrek/Computations/Ai/ICSHM/Predictions/Photos/Images', ImageResizer(RES_X,RES_Y,'/Users/piotrek/Computations/Ai/ICSHM/Predictions/Photos/PredictionPhotos'))
+        imgRGB_conv  = ICSHM_RGB_4_Converter(RES_X, RES_Y)    # konwersja na pliki npy - jak sa, to juz tego nie robi
+        data_manager = ICSHMDataManager(IMAGES_SOURCE_PATH) # na razie nie wiadomo
+        data_manager.convert_data_to_numpy_format( imgRGB_conv, TRAIN_IMAGES_PATH )  # powinno sie nie uruchamiac, jak sa npy
+
+        # weights = compute_class_weights(y)
+        print("Class Weights:", data_manager.weights)
+        print("Sum   Weights:", sum(data_manager.weights))
+
+        # class weights reflects not only pixel ratio but also class importance
+        #class_weights = np.array([0.05, 0.1, 0.1, 0.1, 0.2, 0.5, 1.0, 0.05])
+
+        class_weights = np.array([0.07, 0.33, 0.35, 0.25])
+
+        # Normalize the weights so that their sum is 1
+        normalized_class_weights = class_weights / np.sum(class_weights)
+
+        loss_fn = weighted_categorical_crossentropy(normalized_class_weights)
 
 
-    # Testowanie na danych testowych (nie walidacyjnych)
-    trainer.test_model(test_gen,test_dmg_segmentation)
-    dfs, df = trainer.compute_gen_measures(test_gen,class_weights,CLASS_NAMES)
+        # Przetwarzanie danych do trenowania i stworzenie obiektu trenera (może być niepotrzebny)
+        dataSource = DataSource( TRAIN_IMAGES_PATH, train_ratio=0.7, validation_ratio=0.1, sampleSize=-1)
+        trainer = DLTrainer(CURRENT_MODEL_NAME, model, TASK_PATH)  # Tu wchodzi model, ale można dać "none" i będzie próbował model wydobyć z katalogu
+
+        model=trainer.model  # Gdyby model powyżej nie był podany ("none" - jak w komentarzu), to tutaj go "wydobywamy"
+
+        # Kompilacja modelu i wyswitlenie informacji:
+        #model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE), loss=loss_fn, metrics=[tf.keras.metrics.CategoricalAccuracy(), tf.keras.metrics.MeanIoU(N_CLASSES)])
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE), loss="categorical_crossentropy", metrics=[tf.keras.metrics.CategoricalAccuracy(), tf.keras.metrics.MeanIoU(N_CLASSES)])
+        model.summary()
+
+        # Generatory danych do trenowania (podstawia dane, jak w tablicy) i walidacji:
+        train_gen = DataGeneratorFromNumpyFilesMem(dataSource.get_train_set_files(),BATCH_SIZE,(RES_Y,RES_X),(RES_Y,RES_X),N_CHANNELS,N_CLASSES, augmentation_fn=augment_fn)
+        validation_gen = DataGeneratorFromNumpyFilesMem(dataSource.get_validation_set_files(),1,(RES_Y,RES_X),(RES_Y,RES_X),N_CHANNELS,N_CLASSES)
+        test_gen = DataGeneratorFromNumpyFilesMem(dataSource.get_test_set_files(),1,(RES_Y,RES_X),(RES_Y,RES_X),N_CHANNELS,N_CLASSES)
+
+        # Rozpoczęcie treningu (w używania wytrenowanego modelu komentujemy funkcje poniżej)
+        trainer.train(train_gen, validation_gen, EPOCHS, BATCH_SIZE)
+        trainer.plot_training_history()
+
+        # Poniższe funkcje są używane tylko w przypadku trenowania nowych modeli
+        print("Evaluate on test data")
+        results = model.evaluate(test_gen, batch_size=1)
+        print("test results:", results)
 
 
-    with pd.ExcelWriter(TASK_PATH+'/' + CURRENT_MODEL_NAME + '.xlsx', engine='openpyxl') as writer:
-        dfs.to_excel(writer, sheet_name='ICSHM', index=False)
-        df.to_excel(writer, sheet_name='ICSHM', index=False, startrow=10, startcol=0)
+        # Testowanie na danych testowych (nie walidacyjnych)
+        trainer.test_model(test_gen,test_dmg_segmentation)
+        dfs, df = trainer.compute_gen_measures(test_gen,class_weights,CLASS_NAMES)
 
-    #gener_test(os.path.join( '/Users/piotrek/Computations/Ai/ICSHM/Previews', CURRENT_MODEL_NAME), train_gen, scope=100)
-    #trainer.predict('/home/piotrek/Computations/Ai/ICSHM/Photos/PredictionPhotos',write_prediction_segmentated2)
+
+        with pd.ExcelWriter(TASK_PATH+'/' + CURRENT_MODEL_NAME + '.xlsx', engine='openpyxl') as writer:
+            dfs.to_excel(writer, sheet_name='ICSHM', index=False)
+            df.to_excel(writer, sheet_name='ICSHM', index=False, startrow=10, startcol=0)
+
+        #gener_test(os.path.join( '/Users/piotrek/Computations/Ai/ICSHM/Previews', CURRENT_MODEL_NAME), train_gen, scope=100)
+        #trainer.predict('/home/piotrek/Computations/Ai/ICSHM/Photos/PredictionPhotos',write_prediction_segmentated2)
+
+    finally:
+        # Cleanup
+        import tensorflow.keras.backend as K
+        import gc
+        del model, trainer, train_gen, validation_gen, test_gen
+        K.clear_session()
+        gc.collect()
 
 
 rgb_model_function( 'ICSHM_RGB_DEEPLABV3p_200es', model_deeplabv3p, None, batch_size=32, epochs=200)
