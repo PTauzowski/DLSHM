c1fe96c961d288b5428f62aef144d357e68e745b
piotrek <ptauzow@ippt.pan.pl>
2025-05-21 00:28:52 +0200

    small models test

--- pyScripts/icshm_tasks_small.py
+++ pyScripts/icshm_tasks_small.py
@@ -79,74 +79,74 @@ keras.config.disable_traceback_filtering()
 
 
 
-TASK_NAME='ICSHM_STRUCT_UNET_rn18_small'
-create_unet_fn = lambda: sm.Unet("resnet18", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=4, activation="softmax")
-create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='STRUCTsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
-multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
+# TASK_NAME='ICSHM_STRUCT_UNET_rn18_small'
+# create_unet_fn = lambda: sm.Unet("resnet18", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=4, activation="softmax")
+# create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='STRUCTsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
+#
+#
+# TASK_NAME='ICSHM_DMG_UNET_rn18_small'
+# create_unet_fn = lambda: sm.Unet("resnet18", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=3, activation="softmax")
+# #create_unet_fn = lambda: custom_unet(input_shape=(RES_Y,RES_X,3), num_layers=6, filters=24, num_classes=3, output_activation="softmax")
+# create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='DMGsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE  , augmentations=augmentations)
+#
+#
+# TASK_NAME='ICSHM_STRUCT_UNET_rn101_small'
+# create_unet_fn = lambda: sm.Unet("resnet101", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=4, activation="softmax")
+# create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='STRUCTsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
+
+#
+# TASK_NAME='ICSHM_DMG_UNET_rn101_small'
+# create_unet_fn = lambda: sm.Unet("resnet101", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=3, activation="softmax")
+# create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='DMGsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
+#
+#
+#
+# TASK_NAME='ICSHM_STRUCT_UNET_inceptionv3_small'
+# create_unet_fn = lambda: sm.Unet("inceptionv3", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=4, activation="softmax")
+# create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename,TRAIN_DIR='STRUCTsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# # multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
+#
+#
+# TASK_NAME='ICSHM_DMG_UNET_inceptionv3_small'
+# create_unet_fn = lambda: sm.Unet("inceptionv3", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=3, activation="softmax")
+# create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='DMGsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
+
+# TASK_NAME='ICSHM_STRUCT_CUSTOM_UNET_small'
+# create_unet_fn = lambda: custom_unet(input_shape=(RES_Y,RES_X,3), num_layers=5, filters=24, num_classes=4, output_activation="softmax")
+# create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='STRUCTsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
+#
+#
+# TASK_NAME='ICSHM_DMG_CUSTOM_UNET_small'
+# create_unet_fn = lambda: custom_unet(input_shape=(RES_Y,RES_X,3), num_layers=5, filters=24, num_classes=3, output_activation="softmax")
+# create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename,TRAIN_DIR='DMGsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
+#
+#
+# TASK_NAME='ICSHM_STRUCT_DEEPLABV3p_np_small'
+# create_model_fn = lambda:  DeeplabV3Plus((RES_Y, RES_X, 3), 4, output_activation="softmax",is_pretrained=False)
+# create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename,TRAIN_DIR='STRUCTsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# multi_augmentation_training_structural(TASK_NAME, create_model_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
 
 
-TASK_NAME='ICSHM_DMG_UNET_rn18_small'
-create_unet_fn = lambda: sm.Unet("resnet18", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=3, activation="softmax")
-#create_unet_fn = lambda: custom_unet(input_shape=(RES_Y,RES_X,3), num_layers=6, filters=24, num_classes=3, output_activation="softmax")
-create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='DMGsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
-multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE  , augmentations=augmentations)
+# TASK_NAME='ICSHM_DMG_DEEPLABV3p_np_small'
+# create_model_fn = lambda: DeeplabV3Plus((RES_Y, RES_X, 3), 3, output_activation="softmax",is_pretrained=False)
+# create_dmg_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='DMGsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# multi_augmentation_training_structural(TASK_NAME, create_model_fn, create_dmg_task_fn, BATCH_SIZE, augmentations=augmentations  )
+#
+#
+# TASK_NAME='ICSHM_STRUCT_DEEPLABV3p_small'
+# create_model_fn = lambda:  DeeplabV3Plus((RES_Y, RES_X, 3), 4, output_activation="softmax",is_pretrained=True)
+# create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='STRUCTsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# multi_augmentation_training_structural(TASK_NAME, create_model_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
 
 
-TASK_NAME='ICSHM_STRUCT_UNET_rn101_small'
-create_unet_fn = lambda: sm.Unet("resnet101", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=4, activation="softmax")
-create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='STRUCTsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
-multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
-
-
-TASK_NAME='ICSHM_DMG_UNET_rn101_small'
-create_unet_fn = lambda: sm.Unet("resnet101", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=3, activation="softmax")
-create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='DMGsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
-multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
-
-
-
-TASK_NAME='ICSHM_STRUCT_UNET_inceptionv3_small'
-create_unet_fn = lambda: sm.Unet("inceptionv3", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=4, activation="softmax")
-create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename,TRAIN_DIR='STRUCTsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
-multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
-
-
-TASK_NAME='ICSHM_DMG_UNET_inceptionv3_small'
-create_unet_fn = lambda: sm.Unet("inceptionv3", input_shape=(RES_Y, RES_X, 3), encoder_weights="imagenet", classes=3, activation="softmax")
-create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='DMGsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
-multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
-
-TASK_NAME='ICSHM_STRUCT_CUSTOM_UNET_small'
-create_unet_fn = lambda: custom_unet(input_shape=(RES_Y,RES_X,3), num_layers=6, filters=16, num_classes=4, output_activation="softmax")
-create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='STRUCTsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=B)
-multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
-
-
-TASK_NAME='ICSHM_DMG_CUSTOM_UNET_small'
-create_unet_fn = lambda: custom_unet(input_shape=(RES_Y,RES_X,3), num_layers=6, filters=16, num_classes=3, output_activation="softmax")
-create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename,TRAIN_DIR='DMGsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
-multi_augmentation_training_structural(TASK_NAME, create_unet_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
-
-
-TASK_NAME='ICSHM_STRUCT_DEEPLABV3p_np_small'
-create_model_fn = lambda:  DeeplabV3Plus((RES_Y, RES_X, 3), 4, output_activation="softmax",is_pretrained=False)
-create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename,TRAIN_DIR='STRUCTsmall', RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
-multi_augmentation_training_structural(TASK_NAME, create_model_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
-
-
-TASK_NAME='ICSHM_DMG_DEEPLABV3p_np_small'
-create_model_fn = lambda: DeeplabV3Plus((RES_Y, RES_X, 3), 3, output_activation="softmax",is_pretrained=False)
-create_dmg_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='DMGsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
-multi_augmentation_training_structural(TASK_NAME, create_model_fn, create_dmg_task_fn, BATCH_SIZE, augmentations=augmentations  )
-
-
-TASK_NAME='ICSHM_STRUCT_DEEPLABV3p_small'
-create_model_fn = lambda:  DeeplabV3Plus((RES_Y, RES_X, 3), 4, output_activation="softmax",is_pretrained=True)
-create_struct_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_structural_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='STRUCTsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
-multi_augmentation_training_structural(TASK_NAME, create_model_fn, create_struct_task_fn, BATCH_SIZE, augmentations=augmentations  )
-
-
-TASK_NAME='ICSHM_DMG_DEEPLABV3p_small'
-create_model_fn = lambda: DeeplabV3Plus((RES_Y, RES_X, 3), 3, output_activation="softmax",is_pretrained=True)
-create_dmg_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='DMGsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
-multi_augmentation_training_structural(TASK_NAME, create_model_fn, create_dmg_task_fn, BATCH_SIZE, augmentations=augmentations  )
+# TASK_NAME='ICSHM_DMG_DEEPLABV3p_small'
+# create_model_fn = lambda: DeeplabV3Plus((RES_Y, RES_X, 3), 3, output_activation="softmax",is_pretrained=True)
+# create_dmg_task_fn = lambda model_basename, model, augmentation_fn, BS : ICSHM_damage_task(model=model, TASK_PATH=TASK_PATH, SOURCE_PATH=SOURCE_PATH, TASK_NAME=model_basename, TRAIN_DIR='DMGsmall',RES_X=RES_X, RES_Y=RES_Y, BATCH_SIZE=BS)
+# multi_augmentation_training_structural(TASK_NAME, create_model_fn, create_dmg_task_fn, BATCH_SIZE, augmentations=augmentations  )
